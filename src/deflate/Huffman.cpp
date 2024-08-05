36a65339a7e7822e2a9b6a7aa2c2e1332dd6343e
l09dm0zgus <cx9ps3@gmail.com>
2024-08-05 18:30:10 +0300

    Added creating fixed huffman codes.Added building huffman tree and generating canonical codes.

---
+++ src/deflate/Huffman.cpp
@@ -0,0 +1,222 @@
+//
+// Created by cx9ps3 on 01.08.2024.
+//
+
+#include "Huffman.hpp"
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <iostream>
+#include <queue>
+#include <stack>
+
+std::pair<std::vector<std::uint16_t>, std::vector<uint16_t>> deflate::Huffman::lz77MatchesToVectors(const std::vector<LZ77::Match> &lz77Matches)
+{
+    std::vector<std::uint16_t> literalsAndLengths;
+    literalsAndLengths.reserve(lz77Matches.size());
+
+    std::vector<std::uint16_t> distances;
+    distances.reserve(lz77Matches.size());
+
+    for (const auto &match: lz77Matches)
+    {
+        if (match.length > 1)
+        {
+            literalsAndLengths.push_back(match.length);
+            distances.push_back(match.distance);
+        }
+        else
+        {
+            literalsAndLengths.push_back(static_cast<std::uint16_t>(match.literal));
+        }
+    }
+
+    return {literalsAndLengths, distances};
+}
+
+std::vector<std::uint32_t> deflate::Huffman::countFrequencies(const std::vector<std::uint16_t> &data, std::uint16_t alphabetSize)
+{
+    std::vector<std::uint32_t> frequencies(alphabetSize, 0);
+
+    for (const auto &symbol: data)
+    {
+        frequencies[symbol]++;
+    }
+    return frequencies;
+}
+deflate::Huffman::TreeNodes deflate::Huffman::buildTree(const std::vector<std::uint32_t> &frequencies)
+{
+    std::priority_queue<std::uint32_t, std::vector<std::uint32_t>, NodeCompare> minimalHeap;
+    TreeNodes treeNodes;
+    treeNodes.reserve(frequencies.size());
+
+    for (std::int16_t i = 0; i < frequencies.size(); i++)
+    {
+        if (frequencies[i] > 0)
+        {
+            Node node;
+            node.frequency = frequencies[i];
+            node.symbol = i;
+            treeNodes.push_back(node);
+            minimalHeap.push(treeNodes.size() - 1);
+        }
+    }
+
+    while (minimalHeap.size() > 1)
+    {
+        auto left = minimalHeap.top();
+        minimalHeap.pop();
+
+        auto right = minimalHeap.top();
+        minimalHeap.pop();
+
+        auto newFrequency = treeNodes[left].frequency + treeNodes[right].frequency;
+        Node node;
+        node.frequency = newFrequency;
+        node.leftChildId = left;
+        node.rightChildId = right;
+        treeNodes.push_back(node);
+
+        auto parentIndex = treeNodes.size() - 1;
+        treeNodes[left].parentId = parentIndex;
+        treeNodes[right].parentId = parentIndex;
+
+        minimalHeap.push(parentIndex);
+    }
+
+    return treeNodes;
+}
+
+void deflate::Huffman::calculateCodesLengths(deflate::Huffman::TreeNodes &treeNodes, std::uint32_t rootIndex)
+{
+    if (rootIndex == -1)
+    {
+        return;
+    }
+
+    std::stack<std::pair<std::uint32_t, std::uint8_t>> stack;
+    stack.emplace(rootIndex, 0);
+
+    while (!stack.empty())
+    {
+        auto [nodesIndex, currentLength] = stack.top();
+        stack.pop();
+
+        auto &node = treeNodes[nodesIndex];
+        if (node.leftChildId == -1 && node.rightChildId == -1)
+        {
+            node.codeLength = currentLength;
+        }
+        else
+        {
+            if (node.rightChildId != -1)
+            {
+                stack.emplace(node.rightChildId, currentLength + 1);
+            }
+            if (node.leftChildId != -1)
+            {
+                stack.emplace(node.leftChildId, currentLength + 1);
+            }
+        }
+    }
+}
+
+std::vector<std::byte> deflate::Huffman::encodeWithDynamicCodes(const std::vector<LZ77::Match> &lz77CompressedData, bool isLastBlock)
+{
+    std::vector<std::byte> compressedData;
+    compressedData.reserve(lz77CompressedData.size());
+    auto [literalsAndLengths, distances] = lz77MatchesToVectors(lz77CompressedData);
+
+    auto frequencies = countFrequencies(literalsAndLengths, LITERALS_AND_LENGTHS_ALPHABET_SIZE);
+    auto treeNodes = buildTree(frequencies);
+    calculateCodesLengths(treeNodes, treeNodes.size() - 1);
+
+    std::ranges::sort(treeNodes,NodeSort());
+    std::vector<std::uint8_t> codeLengths = {3, 3, 3, 3,3, 2, 4, 4  };
+    auto codeTable = createCodeTable(codeLengths);
+
+
+    return compressedData;
+}
+
+std::unordered_map<std::uint16_t, std::uint16_t> deflate::Huffman::createCodeTable(const std::vector<std::uint8_t> &codeLengths)
+{
+    std::unordered_map<std::uint16_t, std::uint16_t> codeTable;
+    codeTable.reserve(codeLengths.size());
+
+    std::array<std::uint16_t, MAX_BITS + 1> codeLengthsCount = {0};
+    std::array<std::uint16_t, MAX_BITS + 1> nextCode = {0};
+
+    for(const auto& length : codeLengths)
+    {
+        codeLengthsCount[length]++;
+    }
+
+    std::uint16_t code{0};
+    for(std::uint8_t bit = 1; bit <= MAX_BITS; bit++)
+    {
+        code = (code + codeLengthsCount[bit - 1]) << 1;
+        nextCode[bit] = code;
+    }
+
+
+    for (std::uint32_t i = 0; i < codeLengths.size(); i++)
+    {
+        auto length = codeLengths[i];
+        if (length != 0)
+        {
+            codeTable[nextCode[length]] = 0;
+            nextCode[length]++;
+        }
+    }
+
+    return codeTable;
+}
+
+std::unordered_map<std::uint16_t, std::uint16_t> deflate::Huffman::createReverseCodeTable(const std::vector<std::uint8_t> &codeLengths)
+{
+    auto codeTable = createCodeTable(codeLengths);
+    std::unordered_map<std::uint16_t, std::uint16_t> reverseCodeTable;
+    reverseCodeTable.reserve(codeLengths.size());
+
+    for (const auto &[code, symbol]: codeTable)
+    {
+        reverseCodeTable[code] = symbol;
+    }
+    return reverseCodeTable;
+}
+
+std::vector<std::byte> deflate::Huffman::encodeWithFixedCodes(const std::vector<LZ77::Match> &lz77CompressedData, bool isLastBlock)
+{
+    std::vector<std::byte> compressedData;
+    compressedData.reserve(lz77CompressedData.size());
+
+    //write header
+    std::byte byteToWrite{0};
+    if(isLastBlock)
+    {
+        byteToWrite |= std::byte(0b1);
+    }
+
+    byteToWrite |= (std::byte{0b01} << 2);
+
+    for(int i = 0; i < 8;i++)
+    {
+        std::uint8_t mask = (1 << i + 1) -1 ;
+        std::cout << std::bitset<8>(static_cast<char>(mask)).to_string() << "\n";
+    }
+
+
+    for(const auto& lz77Match : lz77CompressedData)
+    {
+        if(lz77Match.length > 1)
+        {
+            const auto& lengthCode = FIXED_LENGTHS_CODES[lz77Match.length];
+            const auto& distanceCode = FIXED_DISTANCES_CODES[lz77Match.distance];
+            std::cout <<"Code : " << std::bitset<8>(static_cast<char>(distanceCode.code)).to_string() << " ,distance:  " << lz77Match.distance <<"=" << distanceCode.distance << " extra bits: " << std::bitset<8>(distanceCode.extraBits).to_string() << "\n";
+            std::cout <<"Code : " << std::bitset<8>(static_cast<char>(lengthCode.code)).to_string() << " ,length:  " << lz77Match.length <<"=" << static_cast<int>(lengthCode.length) << " extra bits: " << std::bitset<8>(lengthCode.extraBits).to_string() << " extra bits count : "<< static_cast<int>(lengthCode.extraBitsCount)<<"\n";
+        }
+    }
+
+    return compressedData;
+}
---
+++ src/deflate/Huffman.hpp
@@ -0,0 +1,195 @@
+//
+// Created by cx9ps3 on 01.08.2024.
+//
+
+#pragma once
+#include "LZ77.hpp"
+#include <array>
+#include <unordered_map>
+
+namespace deflate
+{
+    class Huffman
+    {
+    private:
+        struct Node
+        {
+            std::uint32_t parentId{0};
+            std::int32_t leftChildId{-1};
+            std::int32_t rightChildId{-1};
+            std::uint16_t code{0};
+            std::int16_t symbol{-1};
+            std::uint8_t codeLength{0};
+            std::uint32_t frequency{0};
+        };
+
+        struct NodeCompare
+        {
+            bool operator()(const std::uint32_t left, const std::uint32_t right) const
+            {
+                return left > right;
+            }
+        };
+
+        struct NodeSort
+        {
+            bool operator()(const Node &left, const Node &right)
+            {
+                if (left.codeLength == right.codeLength)
+                {
+                    return left.symbol < right.symbol;
+                }
+
+                return left.codeLength < right.codeLength;
+            }
+        };
+
+        struct LiteralCode
+        {
+            std::uint16_t code;
+            std::uint8_t  codeLength;
+        };
+
+        struct LengthCode
+        {
+            std::uint16_t length;
+            std::uint16_t code;
+            std::uint8_t  codeLength;
+            std::uint8_t extraBitsCount;
+            std::uint16_t extraBits;
+        };
+        struct DistanceCode
+        {
+            std::uint8_t code;
+            std::uint16_t  distance;
+            std::uint8_t extraBitsCount;
+            std::uint16_t extraBits;
+        };
+
+        using TreeNodes = std::vector<Node>;
+        static constexpr std::uint16_t LITERALS_AND_LENGTHS_ALPHABET_SIZE = 286;
+        static constexpr auto FIXED_LITERALS_CODES{
+                []() constexpr
+                {
+                    std::array<LiteralCode,256> result{};
+                    std::uint16_t start = 0b00110000;
+                    std::uint8_t countOfBits = 8;
+                    for (auto i = 0; i < 256; ++i)
+                    {
+                        if(i == 144)
+                        {
+                            countOfBits = 9;
+                            start = 0b110010000;
+                        }
+
+                        result[i].code = start;
+                        result[i].codeLength = countOfBits;
+                        start++;
+                    }
+                    return result;
+                }()};
+
+
+        static constexpr std::uint16_t DISTANCES_ALPHABET_SIZE = 30;
+        static constexpr auto FIXED_DISTANCES_CODES{
+                []() constexpr
+                {
+                    std::array<DistanceCode, 32769> result{};
+                    std::uint8_t bits{0};
+                    std::uint16_t mask = 0;
+                    std::uint16_t lastDistance = 1;
+                    for (std::uint8_t i = 0; i < DISTANCES_ALPHABET_SIZE; i++)
+                    {
+                        if ((i > 2) && (i % 2) == 0)
+                        {
+                            bits++;
+                            for (std::uint8_t j = 0; j <bits; j++)
+                            {
+                                mask = (1 << j + 1) - 1;
+                            }
+                        }
+
+                        for (std::uint16_t j = 0; j <= mask; j++)
+                        {
+                            DistanceCode code{};
+
+                            code.code = i;
+                            code.distance = lastDistance;
+                            code.extraBitsCount = bits;
+                            code.extraBits = j;
+
+                            result[lastDistance] = code;
+                            lastDistance++;
+                        }
+                    }
+                    return result;
+                }()};
+
+        static constexpr auto FIXED_LENGTHS_CODES {
+                []() constexpr
+                {
+                    std::array<LengthCode,259> result{};
+                    std::uint16_t start = 0b0000000;
+                    std::uint8_t countOfBits = 7;
+                    std::uint8_t extraBitsCount{0};
+                    std::uint16_t length{2};
+                    std::uint8_t mask{0};
+                    for (auto i = 256; i < 285; ++i)
+                    {
+                        if(i == 280)
+                        {
+                            countOfBits = 8;
+                            start = 0b11000000;
+                        }
+                        if(i < 265)
+                        {
+                            length++;
+                        }
+                        else
+                        {
+                            if((i % 4) == 1)
+                            {
+                                extraBitsCount++;
+
+                            }
+                        }
+
+                        for (std::uint8_t j = 0; j < extraBitsCount; j++)
+                        {
+                            mask = (1 << j + 1) - 1;
+                        }
+
+                        for (std::uint16_t j = 0; j <= mask; j++)
+                        {
+                            result[length].length = length;
+                            result[length].code = start;
+                            result[length].codeLength = countOfBits;
+                            result[length].extraBits = j;
+                            result[length].extraBitsCount = extraBitsCount;
+                            if(i > 264)
+                            {
+                                length++;
+                            }
+                        }
+                        start++;
+                    }
+
+                    result[258].extraBitsCount = 0;
+                    result[258].extraBits = 0;
+
+                    return result;
+                }()};
+
+        static constexpr std::uint8_t MAX_BITS = 15;
+        static std::pair<std::vector<std::uint16_t>, std::vector<uint16_t>> lz77MatchesToVectors(const std::vector<LZ77::Match> &lz77Matches);
+        static std::vector<std::uint32_t> countFrequencies(const std::vector<std::uint16_t> &data, std::uint16_t alphabetSize);
+        static TreeNodes buildTree(const std::vector<std::uint32_t> &frequencies);
+        static void calculateCodesLengths(TreeNodes &treeNodes, std::uint32_t rootIndex);
+        static std::unordered_map<std::uint16_t, std::uint16_t> createCodeTable(const std::vector<std::uint8_t> &codeLengths);
+        static std::unordered_map<std::uint16_t, std::uint16_t> createReverseCodeTable(const std::vector<std::uint8_t> &codeLengths);
+
+    public:
+        static std::vector<std::byte> encodeWithDynamicCodes(const std::vector<LZ77::Match> &lz77CompressedData, bool isLastBlock);
+        static std::vector<std::byte> encodeWithFixedCodes(const std::vector<LZ77::Match> &lz77CompressedData, bool isLastBlock);
+    };
+}// namespace deflate
