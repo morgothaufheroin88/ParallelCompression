c1fc01cf48cb8d45ebc63d8199058b13ebc4224d
l09dm0zgus <cx9ps3@gmail.com>
2024-11-04 18:10:36 +0200

    Added crc32 using intrinsic is processor has supports and standard implementation.

---
+++ src/compression/parallel/CRC32.hpp
@@ -0,0 +1,44 @@
+//
+// Created by cx9ps3 on 04.11.2024.
+//
+
+#pragma once
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+#ifdef SSE42_SUPPORTED
+#include <smmintrin.h>
+#endif
+
+namespace deflate
+{
+#if defined(__clang__) || defined(__GNUC__)
+    __attribute__((target("crc32")))
+#endif
+
+    inline std::uint32_t crc32(const std::vector<std::byte> &data)
+    {
+        std::uint32_t crc = 0xFF'FF'FF'FFU;
+
+#ifdef SSE42_SUPPORTED
+        for(const auto byte : data)
+        {
+             crc = _mm_crc32_u8(crc, static_cast<std::uint8_t>(byte));
+        }
+        return crc ^ 0xFF'FF'FF'FFU;
+#elif
+
+        for(const auto byte : data)
+        {
+            crc ^= static_cast<std::uint8_t>(byte);
+            for (std::uint8_t k = 0; k < 8; ++k)
+            {
+                constexpr std::uint32_t POLY = 0x82'F6'3B'78U;
+                crc = static_cast<bool>(crc & 1) ? ((crc >> 1) ^ POLY) : (crc >> 1);
+            }
+        }
+
+        return ~crc;
+#endif
+    }
+}// namespace deflate
